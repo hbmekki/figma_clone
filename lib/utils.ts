6cd81c6a1f6c9edfca7ae1f2d2567cccfcdc4d87
Hammouda Ben Mekki <hbmekki@gmail.com>
2024-08-18 10:56:04 -0400

    Update utils.ts
    .

--- lib/utils.ts
+++ lib/utils.ts
@@ -50,49 +50,49 @@ export const getShapeInfo = (shapeType: string) => {
   switch (shapeType) {
     case "rect":
       return {
-        icon: "/assets/rectangle.svg",
+        icon: "./assets/rectangle.svg",
         name: "Rectangle",
       };
 
     case "circle":
       return {
-        icon: "/assets/circle.svg",
+        icon: "./assets/circle.svg",
         name: "Circle",
       };
 
     case "triangle":
       return {
-        icon: "/assets/triangle.svg",
+        icon: "./assets/triangle.svg",
         name: "Triangle",
       };
 
     case "line":
       return {
-        icon: "/assets/line.svg",
+        icon: "./assets/line.svg",
         name: "Line",
       };
 
     case "i-text":
       return {
-        icon: "/assets/text.svg",
+        icon: "./assets/text.svg",
         name: "Text",
       };
 
     case "image":
       return {
-        icon: "/assets/image.svg",
+        icon: "./assets/image.svg",
         name: "Image",
       };
 
     case "freeform":
       return {
-        icon: "/assets/freeform.svg",
+        icon: "./assets/freeform.svg",
         name: "Free Drawing",
       };
 
     default:
       return {
-        icon: "/assets/rectangle.svg",
+        icon: "./assets/rectangle.svg",
         name: shapeType,
       };
   }
