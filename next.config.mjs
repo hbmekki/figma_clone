a59fef7962a77e450f18aa3788634299a1287353
Hammouda Ben Mekki <hbmekki@gmail.com>
2024-08-22 16:37:22 -0400

    use env variable for a prefix using github env

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,10 @@
 
 const nextConfig = {
 
-    basePath: process.env.GITHUB_ACTIONS?  `${process.env?.GITHUB_REPOSITORY.replace(/.*?\//, '')}`: '',
-    assetPrefix: process.env.GITHUB_ACTIONS?  `${process.env?.GITHUB_REPOSITORY.replace(/.*?\//, '')}`: '',
+    basePath: process.env.GITHUB_ACTIONS?  `${process.env?.GITHUB_REPOSITORY.replace(/.*?\//, '/')}`: '',
+    assetPrefix: process.env.GITHUB_ACTIONS?  `${process.env?.GITHUB_REPOSITORY.replace(/.*?\//, '/')}`: '',
+    // basePath: process.env.NEXT_PUBLIC_PREFIX || '',
+    // assetPrefix: process.env.NEXT_PUBLIC_PREFIX || '',
     output: "export",
     reactStrictMode: true,
 
